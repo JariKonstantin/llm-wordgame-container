dad9dbce5643d33fe84a4fb02d0fb414c1b1c906
JariKonstantin <110922352+JariKonstantin@users.noreply.github.com>
2025-01-29 13:45:47 +0100

    typo fix

--- llm-wordgame/src/app/page.tsx
+++ llm-wordgame/src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
             </Button>
             <Image
               priority
-              src={"/wordhiz/logo.png"}
+              src={"/wordwhiz/logo.png"}
               alt="logo"
               width={250}
               height={250}
